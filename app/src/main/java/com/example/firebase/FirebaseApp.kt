ed08ef27e49f4cf1f2847add1ccc4dfc301d927c
Juan Camacho <camacho@sicar.mx>
2023-11-07 00:36:21 -0600

    add firebase dependencies

---
+++ app/src/main/java/com/example/firebase/FirebaseApp.kt
@@ -0,0 +1,15 @@
+package com.example.firebase
+
+import android.app.Application
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.initialize
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FirebaseApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Firebase.initialize(this)
+    }
+}
