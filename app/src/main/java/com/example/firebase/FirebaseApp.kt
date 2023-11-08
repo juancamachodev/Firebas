fc9d3f4f7b79cf4d6358dc22496c2f1f39581385
Juan Camacho <camacho@sicar.mx>
2023-11-08 11:27:11 -0600

    fix libraries

--- app/src/main/java/com/example/firebase/FirebaseApp.kt
+++ app/src/main/java/com/example/firebase/FirebaseApp.kt
@@ -1,6 +1,7 @@
 package com.example.firebase
 
 import android.app.Application
+import com.google.firebase.FirebaseApp
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.ktx.initialize
 import dagger.hilt.android.HiltAndroidApp
@@ -10,6 +11,6 @@ class FirebaseApp: Application() {
 
     override fun onCreate() {
         super.onCreate()
-        Firebase.initialize(this)
+        FirebaseApp.initializeApp(this)
     }
 }
